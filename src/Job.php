a0607c3909118c500efaf3a955ece1ec48718672
gbleijenberg <gbleijenberg@schueler.hakzell.at>
2024-06-09 15:34:28 +0200

    minor changes

--- src/Job.php
+++ src/Job.php
@@ -13,6 +13,14 @@ class Job
 
     public function setUsername(string $fname, string $lname)
     {
-        $this->username = strtolower(substr($fname, 0 ,1)) . strtolower($lname);
+        $uname = strtolower(substr($fname, 0 ,1)) . strtolower($lname);
+        $uname->CleanUp();
+        $this->username = $uname;
+    }
+
+    public function CleanUp($usname)
+    {
+        $usname = str_replace('ö', 'oe', $usname);
+        $usname = str_replace('ü', 'ue', $usname);
     }
 }
