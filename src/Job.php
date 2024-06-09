239efb8b348d0e61387c914ebe977566237073b2
gbleijenberg <gbleijenberg@schueler.hakzell.at>
2024-06-09 14:40:27 +0200

    init commit

---
+++ src/Job.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Gbleijenberg\SetJob;
+
+class Job
+{
+    private string $username;
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $fname, string $lname)
+    {
+        $this->username = strtolower(substr($fname, 0 ,1)) . strtolower($lname);
+    }
+}
